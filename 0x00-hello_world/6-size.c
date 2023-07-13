ecb43a82377e93a10926970d5a22f327ebebd6ce
Rayinde11 <Rayinde11@gmail.com>
2023-07-13 16:09:39 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,18 @@
-#include<studio.h>
+#include <stdio.h>
+
 /**
- * main - Entry bpoint
+ * main - prints the size of various types.
  *
- * Return:Always 0 (success)
- * \
- * int main(void)
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
-           printf("size of a char: cd byte(s)\n", sizeof(char));
-	   printf("size of an int: cd byte(s)\n", sizeof(int));
-	   printf("size of a long int: cd byte(s)\n", sizeof(long int));
-	   printf("size of a long long int: cd byte(s)\n", sizeof(long long int));
-	   printf("size of a float: cd byte(s)\n", sizeof(float));
-	   return (0
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+
+	return (0);
 }
