5f369ca84004919673a653b5a56693eedd88811a
Rayinde11 <Rayinde11@gmail.com>
2023-07-13 16:17:26 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-#include <u stdio.h >
+#include <stdio.h>
+
 /**
- * main - Leo king of football
- * Return: 0(Sucess)
- * \
- * int main(void)
+ * main -  prints exactly "Programming is like building a multilingual puzzle.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
- * puts("\"Programming is like building a multilingual puzzle");
- * return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
