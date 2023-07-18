45295da0f1da4478787402a60ff2fee3ffac33aa
Rayinde11 <Rayinde11@gmail.com>
2023-07-18 04:42:29 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,28 @@
-#include "main.h"
-/**
- * main - Prints _putchar as a message.
+/*
+ * File: 0-putchar.c
+ * Auth: Brennan D Baraban
  *
- * Return: Always 0 (Success)
  */
 
+#include "0-putchar.c"
+
+/**
+ * main - Prints "0-putchar.c" followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
-
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
+
 	return (0);
 }
