13be270e89b1d55d863245d9db0196e4d07f0090
Rayinde11 <Rayinde11@gmail.com>
2023-07-18 04:59:36 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,28 +1,19 @@
-/*
- * File: 0-putchar.c
- * Auth: Brennan D Baraban
- *
- */
-
-#include "0-putchar.c"
+#include "main.h"
 
 /**
- * main - Prints "0-putchar.c" followed by a new line.
- *
- * Return: Always 0.
+ * main - Print _putchar
+ * Return: Always 0
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
