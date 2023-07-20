52cf37682b486cec63efaa4a50a8487ac772adf1
Rayinde11 <Rayinde11@gmail.com>
2023-07-20 04:37:12 -0700

    moll

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,18 @@
-3-print_numbers.c
+#include "main.h"
+
+/**
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
+*/
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
